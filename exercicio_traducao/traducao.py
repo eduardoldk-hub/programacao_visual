753893fb665fff37d4a64ce9eef008ae98597cec
Eduardoldk <eduardoldk@outlook.com>
2020-05-26 14:35:22 -0300

    Exercicio de tradução e adição em outro arquivo finalizado, utilizando try, except para verificação se a pasta está ou não criada

--- exercicio_traducao/traducao.py
+++ exercicio_traducao/traducao.py
@@ -1,3 +1,4 @@
+# Eduardo de Morais Levandovski.
 # limpar o console -----------------------------------------------------
 import os
 os.system('cls||clear')
@@ -10,6 +11,7 @@ import json
 encoding='utf-8'
 
 
+#Abrindo o arquivo de texto escrito.
 nome_arquivo = "nome_do_arquivo.txt"
 arquivo_texto = open(nome_arquivo)
 
@@ -17,23 +19,31 @@ arquivo_texto = open(nome_arquivo)
 texto_do_arquivo = arquivo_texto.read()
 
 translator = Translator()
+
+#Idioma em que se econtra o texto.
 idioma_do_arquivo = translator.detect(texto_do_arquivo)
-
-
 print(f"Nome do arquivo: {nome_arquivo}\nIdioma do arquivo: {idioma_do_arquivo}\n")
 
 
-tabela_de_idiomas = (json.dumps(googletrans.LANGUAGES))
-print(tabela_de_idiomas)
+#Menu com possiveis idiomas.
+print(json.dumps(googletrans.LANGUAGES, indent=4))
+
+#Solicitação que o usuario escolha uma linguagem.
 idioma_escolhido = str(input("Escolha um dos idiomas acima e digite a silga correspondente: "))
 
+#Tradução do arquivo para a linguagem escolhida.
+traduzindo_arquivo = str(translator.translate(texto_do_arquivo, dest=idioma_escolhido))
 
-traducao_arquivo = translator.translate(texto_do_arquivo, dest=idioma_escolhido)
+#Criando o arquivo caso não exista.
+try:
+    os.makedirs("./exercicio_traducao/nome_do_arquivo_traduzido.txt")
+except:
+    nome_do_arquivo_traduzido = open("nome_do_arquivo_traduzido.txt", "w")
 
-os.makedirs("./exercicio_traducao/nome_do_arquivo_traduzido.txt")
-
+#Salvando o arquivo traduzido no arquivo criado.
+nome_do_arquivo_traduzido.write(traduzindo_arquivo)
 
 
 arquivo_texto.close()
-arquivo_texto_traduzido.close()
+nome_do_arquivo_traduzido.close()
 
