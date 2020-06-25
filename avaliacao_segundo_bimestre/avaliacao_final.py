74dbe39a538f0b4cef66ac36956cff0f100bdabf
Eduardoldk <eduardoldk@outlook.com>
2020-06-24 21:25:18 -0300

    trabalho semi pronto

--- avaliacao_segundo_bimestre/avaliacao_final.py
+++ avaliacao_segundo_bimestre/avaliacao_final.py
@@ -1,17 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import platform
-import time
-import getpass
-import sys
-import sys
-import os
-import random
-
-
-
-
+import platform, time, getpass, sys, os, random, clipboard
 
 #   para não pedir confirmação de uso de camera e microfone 
 chrome_configurações = Options()
@@ -45,6 +35,7 @@ def aluno_visitante(nome_visitante):
         try:
             campo = navegador.find_element_by_id("guest-name")
             campo.send_keys(nome_visitante)
+            time.sleep(5)
         except:
             print(f"[ERROR] Campo nome não encontrado. Outra tentativa acontecerá em {iteracao}")
             time.sleep(iteracao)
@@ -65,7 +56,7 @@ def aluno_visitante(nome_visitante):
 
     #   clica para fechar as configurações de audio e video
     try:
-        time.sleep(10)
+        time.sleep(5)
         campo = navegador.find_element_by_xpath('//*[@id="techcheck-modal"]/button')
         campo.click()
     except:
@@ -76,7 +67,7 @@ def aluno_visitante(nome_visitante):
 
     #   fecha o tutorial do collaborate
     try:
-        time.sleep(10)
+        time.sleep(5)
         campo = navegador.find_element_by_xpath('//*[@id="announcement-modal-page-wrap"]/button')
         campo.click()
     except:
@@ -87,7 +78,7 @@ def aluno_visitante(nome_visitante):
 
     # clica na aba '<<'
     try:
-        time.sleep(10)
+        time.sleep(5)
         campo = navegador.find_element_by_xpath('//*[@id="side-panel-open"]')
         campo.click()
     except:
@@ -98,7 +89,7 @@ def aluno_visitante(nome_visitante):
 
     # clica na aba todos
     try:
-        time.sleep(10)
+        time.sleep(5)
         campo = navegador.find_element_by_xpath('//*[@id="chat-channel-scroll-content"]/ul/li/ul/li')
         campo.click()
     except:
@@ -121,7 +112,6 @@ def aluno_visitante(nome_visitante):
             valor = random.choice(frases)
             while valor in aux:
                 valor = random.choice(frases)
-                time.sleep(3)
 
             # clicar no campo de mensagens e envia a mensagem
             while iteracao > 0:
@@ -140,21 +130,6 @@ def aluno_visitante(nome_visitante):
                     print('[OK] - Mensagem enviada com sucesso')
                     iteracao = 0
             iteracao = 1
-
-    # clica no quadro de participantes
-    time.sleep(2)
-    iteracao = 1
-    while iteracao > 0:
-        try:
-            campo = navegador.find_element_by_xpath('//*[@id="panel-control-participants"]')
-            campo.click()
-        except:
-            print(f'[ERROR] - Botão não encontrado, nova tentativa em: {iteracao}')
-            time.sleep(iteracao)
-            iteracao += 1
-        else:
-            print('[OK] - Clique no quadro de participantes.')
-            iteracao = 0
 
     #   clica no campo chat
     time.sleep(2)
@@ -192,7 +167,7 @@ def aluno_visitante(nome_visitante):
             campo = navegador.find_element_by_xpath('//*[@id="panel-chatchannelselector-content"]/header/div/div/div/input')
             campo.click()
             campo.send_keys('EDUARDO DE MORAIS LEVANDOVSKI')
-            time.sleep(4)
+            time.sleep(5)
             campo = navegador.find_element_by_xpath('//*[@id="chat-search-dropdown"]/li[2]/button/span')
             campo.click()
         except:
@@ -219,6 +194,96 @@ def aluno_visitante(nome_visitante):
             iteracao += 1
         else:
             print('[OK] - Mensagem enviada.')
+    
+    #   clica no voltar
+    time.sleep(3)
+    try:
+        campo = navegador.find_element_by_xpath('//*[@id="panel-back-button"]')
+        campo.click()
+    except:
+        print("[ERROR] - Não foi possivel clicar no campo voltar.")
+    else:
+        print("[OK] - Clique no botão voltar.")
+    
+    #   clica no chat de todos
+    time.sleep(3)
+    try:
+        campo = navegador.find_element_by_xpath('//*[@id="chat-channel-scroll-content"]/ul/li/ul/li')
+        campo.click()
+    except:
+        print("[ERROR] - Não foi possivel clicar no chat TODOS.")
+    else:
+        print("[OK] - Clique no chat TODOS.")
+
+    #   clica no campo de mensagem e envia
+    try:
+        campo = navegador.find_element_by_id('message-input')
+        campo.click()
+        campo.send_keys("Professor minha internet está ruim.:pensive:")
+        campo.send_keys(Keys.ENTER)
+        time.sleep(4)
+        campo.send_keys("Não estou conseguindo acompanhar vou ter que sair.")
+        campo.send_keys(Keys.ENTER)
+        time.sleep(4)
+        campo.send_keys(":right_facing_fist: :left_facing_fist:")
+        campo.send_keys(Keys.ENTER)
+    except:
+        print("[ERROR] - Não foi possivel clicar no campo de digitação e enviar mensagem.")
+    else:
+        print("[OK] - Mensagem enviada.")
+
+    #   clica na aba da esquerda no collaborate
+    time.sleep(4)
+    try:
+        campo = navegador.find_element_by_xpath('//*[@id="session-menu-open"]')
+        campo.click()
+    except:
+        print("[ERROR] - Não foi possivel abrir a janela lateral do collaborate")
+    else:
+        print("[OK] - Janela lateral do collaborate aberta.")
+
+    #   clica no icone para deixar a sessão
+    time.sleep(5)
+    try:
+        campo = navegador.find_element_by_xpath('//*[@id="leave-session"]')
+        campo.click()
+    except:
+        print("[ERROR] - Não foi possivel clicar no icone para deixar a aula.")
+    else:
+        print("[OK] - Clique no icone para deixar a sessão.")
+    
+    #   clica no voto 
+    time.sleep(5)
+    try:
+        campo = navegador.find_element_by_xpath('//*[@id="session-exit-modal"]/div[2]/div[1]/div/ul/li[1]/div[1]/div/img')
+        campo.click()
+    except:
+        print("[ERROR] - Não foi possivel votar.")
+    else:
+        print("[OK] - Voto efetuado.")
+    
+    #   justifica o voto
+    time.sleep(3)
+    try:
+        campo = navegador.find_element_by_xpath('//*[@id="session-exit-modal"]/div[2]/div[1]/div[2]/div/div[1]/ul/li[1]/label')
+        campo.click()
+    except:
+        print("[ERROR] - Não foi possivel justificar o voto.")
+    else:
+        print("[OK] - Voto justificado")
+    
+    #   confirma o voto
+    time.sleep(2)
+    try:
+        campo = navegador.find_element_by_xpath('//*[@id="session-survey-submit"]')
+        campo.click()
+        time.sleep(5)
+        print("[OK] - Saindo da sessão")
+    except:
+        print("[ERRO] - Não foi possivel deixar a sessão.")
+    else:
+        print("[OK] - Fechando navegador.")
+        navegador.close()
 
 
 #   função para logar no ava-----------------------------------------------------------------------
@@ -289,15 +354,16 @@ def logar_ava(usuario, senha, data):
     try:
         clique = navegador.find_element_by_partial_link_text(data)
         clique.click()
-        time.sleep(5)
     except:
         print(f"[ERROR] - Não foi possivel clicar na aula do dia[{data}].")
     else:
         print(f"[OK] - Abrindo a Aula do dia {data}.")
 
     #clica na sessão
+    time.sleep(2)
     try:
-        navegador.find_element_by_xpath("//div/a[@class='mod-link' and contains(@href, '%s')]").click()
+        clique = navegador.find_element_by_tag_name('//div[@a="Aula"]')
+        clique.click()
     except:
         print("[ERROR] - Não foi possivel entrar na sessão da aula.")
     else:
@@ -338,8 +404,10 @@ def logar_ava(usuario, senha, data):
             iteracao += 1
         else:
             iteracao = 0
-        
-#   apresentação
+
+
+#   apresentação -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 print('''
          ____ __                %%%%%
           { --.\  |          .) Prova %
@@ -363,11 +431,13 @@ print('''
 
 
 #   Digitar se quer entrar como visitante o ou login de aluno.
-tipo_aula = str(input("Deseja entrar como? [egresso/visitante]: "))
+tipo_aula = str(input("Deseja entrar como? [egresso/visitante/professor]: "))
 
 # se entrar como visitante
 if tipo_aula == 'visitante':
     visitante = str(input("Olá visitante! Digite seu nome: "))
+
+    #   abre o google com o driver certo
     try:
         if platform.system() == 'Linux':
             navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_configurações)
@@ -378,6 +448,10 @@ if tipo_aula == 'visitante':
     except:
         print("[ERROR] - Chrome não foi inciado!")
         raise("[FATAL ERROR] - reinicie o programa!")
+    else:
+        print("[OK] - Google iniciado com sucesso.")
+
+    #   abre a pagina do collaborate como visitante.    
     aluno_visitante(visitante)
 
 #   se entrar como aluno
@@ -391,7 +465,7 @@ elif tipo_aula == 'egresso':
     #   numero da aula que quer entrar
     data_aula = str(input("Digite o dia e o mes da aula que quer acessar [00/00]: "))
 
-    #   inicia o google
+    #   abre o drive certo do google
     try:
         if platform.system() == 'Linux':
             navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_configurações)
@@ -402,12 +476,134 @@ elif tipo_aula == 'egresso':
     except:
         print("[ERROR] - Chrome não foi inciado!")
         raise("[FATAL ERROR] - reinicie o programa!")
-    
+    else:
+        print("[OK] - Google iniciado com sucesso.")
+
+    #   loga no ava e faz todas as funções
     logar_ava(usuario_ava, senha_ava, data_aula)
 
+elif tipo_aula == 'professor':
+    nome_professor = str(input("Digite seu login professor: "))
+    print(f"Desculpe professor {nome_professor[2:].title()}, sou um humilde bot que ainda não possui recursos para lhe ajudar.\nPorem, posso evoluir, entre em contato com meu criador e passe a ele os requisitos necessarios,\nele ficará muito feliz em ajudar.")
+    contato = str(input("\nDeseja conversar com Eduardo pelo: LinkedIn[1], E-mail[2], Whatsapp[3], Não quero falar com ele[4]: "))
+
+    #   abre o linkedin
+    if contato == '1':
+
+        #   abre o drive
+        try:
+            if platform.system() == 'Linux':
+                navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_configurações)
+                print("\n[OK] - Chrome iniciado no sistema: Linux")
+            else:
+                navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver.exe', options=chrome_configurações)
+                print("[OK] - Chrome iniciado no sistema: Windows")
+        except:
+            print("[ERROR] - Chrome não foi inciado!")
+            raise("[FATAL ERROR] - reinicie o programa!")
+        else:
+            print("[OK] - Google iniciado com sucesso.")
+        
+        #   abre o linkedin
+        try:
+            campo = navegador.get("https://www.linkedin.com/in/eduardo-levandovski-6a1630160/")
+            campo.click()
+        except:
+            print("[ERROR] - Não foi possivel abrir o LinkedIn")
+        else:
+            print("[OK] - LinkedIn aberto.")
+    
+    #   abre o email
+    elif contato == '2':
+        contatar = str(input("\nPor qual email você gostaria de entrar em contato? Outlook[1], Gmail[2], Outros[3]: "))
+        if contatar == '1':
+            #   abre o drive
+            try:
+                if platform.system() == 'Linux':
+                    navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_configurações)
+                    print("\n[OK] - Chrome iniciado no sistema: Linux")
+                else:
+                    navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver.exe', options=chrome_configurações)
+                    print("[OK] - Chrome iniciado no sistema: Windows")
+            except:
+                print("[ERROR] - Chrome não foi inciado!")
+                raise("[FATAL ERROR] - reinicie o programa!")
+            else:
+                print("[OK] - Google iniciado com sucesso.")
+
+            #   abre o outlook na pagina principal
+            try:   
+                campo = navegador.get("https://outlook.live.com")
+                ctrl_c = clipboard.copy('eduardoldk@outlook.com')
+                time.sleep(10)
+            except:
+                print("[ERROR] - Não foi possivel abrir o Outlook.")
+            else:
+                print("[OK] - Outlook foi aberto, email do Eduardo está salvo no seu ctrl+c")
+
+        elif contatar == '2':
+            #   abre o drive
+            try:
+                if platform.system() == 'Linux':
+                    navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_configurações)
+                    print("\n[OK] - Chrome iniciado no sistema: Linux")
+                else:
+                    navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver.exe', options=chrome_configurações)
+                    print("[OK] - Chrome iniciado no sistema: Windows")
+            except:
+                print("[ERROR] - Chrome não foi inciado!")
+                raise("[FATAL ERROR] - reinicie o programa!")
+            else:
+                print("[OK] - Google iniciado com sucesso.")
+
+            #   abre o gmail do google
+            try:
+                campo = navegador.get("https://mail.google.com/")
+                ctrl_c = 'eduardoldk@outlook.com'
+                clipboard.copy(ctrl_c)
+            except:
+                print("[ERROR] - Não foi possivel abrir o Gmail.")
+            else:
+                print("[OK] - Gmail aberto, email do Eduardo está salvo no seu ctrl+c")
+
+
+        elif contatar == '3':
+            ctrl_c = 'eduardoldk@outlook.com'
+            clipboard.copy(ctrl_c)
+            print("Fique a vontade para escolher a plataforma da qual quer enviar, o email do Eduardo está salvo no seu ctrl+c")
+        else:
+            print("[FATAL ERROR] - Valor inserido invalido.")
+            
+    #   abre o whatsapp
+    elif contato == '3':
+        try:
+            if platform.system() == 'Linux':
+                navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_configurações)
+                print("\n[OK] - Chrome iniciado no sistema: Linux")
+            else:
+                navegador = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver.exe', options=chrome_configurações)
+                print("[OK] - Chrome iniciado no sistema: Windows")
+        except:
+            print("[ERROR] - Chrome não foi inciado!")
+            raise("[FATAL ERROR] - reinicie o programa!")
+        else:
+            print("[OK] - Google iniciado com sucesso.")       
+
+        #   
+        try:
+            campo = navegador.get("https://chat.whatsapp.com/KseQrfvK7Gs2n96FcjF0KY")
+        except:
+            print("Não foi possivel abrir o WhatsApp.")
+        else:
+            print("[OK] - WhatsApp aberto.")
+
+
+    #   Não quero falar com voce
+    elif contato == '4':
+        print("É uma pena. Bem, nos vemos na proxima então! Até um proximo codigo.")
 #   caso não logue com aluno e com visitante.    
 else:
-    entrada = str(input(f'[ERROR] - "{tipo_aula}" - Não é um tipo de entrada aceitavel, voce deve digitar apenas, egresso caso tenha login no AVA\n ou visitante caso não tenha login no AVA, deseja tentar novamente? [s/n]'))
+    entrada = str(input(f'[ERROR] - "{tipo_aula}" - Não é um tipo de entrada aceitavel, voce deve digitar apenas, "egresso" caso tenha login no AVA\n"visitante" caso não tenha login no AVA, ou "professor", caso seja um,\nDeseja tentar novamente? [s/n]'))
     if entrada == 's':
         print("[RELOADING] - O programa irá reiniciar, conte 5 segundos.")
         time.sleep(5)
@@ -415,6 +611,40 @@ else:
     else:
         print("[GOOD BAY] - Até um proximo código.")
 
+
+
+
+print("""
+                               .-.
+                               {{@}}
+               <>               8@8
+             .::::.             888
+         @\\/W\/\/W\//@         8@8
+          \\/^\/\/^\//     _    )8(    _
+           \_O_<>_O_/     (@)__/8@8\__(@)
+      ____________________ `~"-=):(=-"~`
+     |<><><>  |  |  <><><>|     |.|
+     |<>      |  |      <>|     |S|
+     |<>      |  |      <>|     |'|
+     |<>   .--------.   <>|     |.|
+     |     |   ()   |     |     |P|
+     |_____| (O\/O) |_____|     |'|
+     |     \   /\   /     |     |.|
+     |------\  \/  /------|     |U|
+     |       '.__.'       |     |'|
+     |        |  |        |     |.|
+     :        |  |        :     |N|
+      \<>     |  |     <>/      |'|
+       \<>    |  |    <>/       |.|
+        \<>   |  |   <>/        |K|
+         `\<> |  | <>/'         |'|
+     jgs   `-.|  |.-`           \ /
+              '--'               ^
+ 
+ 
+Espero que o codigo tenha sido divertido de usar.
+Bom dia/Boa noite!.
+""")
 
 
 
